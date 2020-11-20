a68edd3869541d2b4a1452c11f62f03a12c79e87
nhatvu148 <nhatvu148@gmail.com>
2020-11-20 17:22:32 +0900

    adding definition link

---
+++ src/document.ts
@@ -0,0 +1,58 @@
+const open = require("open");
+const open_darwin = require("mac-open");
+
+export default class Document {
+  // decide what os should be used
+  // possible node values 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+  private static platform: string = process.platform;
+
+  private static defaultDocs: object = {
+    ahk: "https://autohotkey.com/docs/commands/${query}.htm",
+    controller: "http://api.rubyonrails.org/?q=${query}",
+    rb: "http://ruby-doc.com/search.html?q=${query}",
+    js: "https://developer.mozilla.org/en-US/search?q=${query}&topic=js",
+    html: "https://developer.mozilla.org/en-US/search?q=${query}&topic=html",
+    htm: "https://developer.mozilla.org/en-US/search?q=${query}&topic=html",
+    coffee: "https://developer.mozilla.org/en-US/search?q=${query}",
+    php: "http://php.net/manual-lookup.php?pattern=${query}",
+    clj: "http://clojuredocs.org/search?x=0&y=0&q=${query}",
+    go: "http://golang.org/search?q=${query}",
+    c: "http://www.cplusplus.com/search.do?q=${query}",
+    cpp: "http://www.cplusplus.com/search.do?q=${query}",
+    smarty: "http://www.smarty.net/${query}",
+    cmake: "http://cmake.org/cmake/help/v2.8.8/cmake.html#command:${query}",
+    perl: "http://perldoc.perl.org/search.html?q=${query}",
+    cs: "http://social.msdn.microsoft.com/Search/?query=${query}",
+    lua: "http://pgl.yoyo.org/luai/i/${query}",
+    pgsql:
+      "http://www.postgresql.org/search/?u=%%2Fdocs%%2Fcurrent%%2F&q=${query}",
+    erl: "http://erldocs.com/R16B03/?search=${query}",
+    hs: "http://hayoo.fh-wedel.de/?query=${query}",
+    scala: "https://www.scala-lang.org/api/current/?search=${query}",
+    css: "http://devdocs.io/#q=${query}",
+    scss: "http://devdocs.io/#q=${query}",
+    less: "http://devdocs.io/#q=${query}",
+    google: "https://google.com/search?q=${query}",
+    py: "http://docs.python.org/3/search.html?q=${query}",
+  };
+
+  public static open(ext: string, keyword: string, customDocs?: object): void {
+    if (!keyword) {
+      return;
+    }
+
+    let docs: any = this.defaultDocs;
+    if (customDocs) {
+      Object.assign(docs, customDocs);
+    }
+
+    let url: string = "";
+    if (ext in docs) {
+      url = docs[ext].replace("${query}", encodeURIComponent(keyword));
+    } else {
+      url = docs["google"].replace("${query}", encodeURIComponent(keyword));
+    }
+
+    this.platform === "darwin" ? open_darwin(url) : open(url);
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import path = require('path');
 import {
   root1,
   root2,
@@ -13,8 +14,53 @@ import {
   psjGuiKeys,
   callTips,
 } from "./data";
+import Document from "./document";
 
 export function activate(context: vscode.ExtensionContext) {
+  const definition = vscode.languages.registerDefinitionProvider("python", {
+    provideDefinition(
+      document: vscode.TextDocument,
+      position: vscode.Position,
+      token: vscode.CancellationToken,
+    ) {
+      // vscode.workspace.getConfiguration()
+      return [
+        new vscode.Location(
+          vscode.Uri.file(`${path.dirname(document.fileName)}/test2.py`),
+          new vscode.Position(1, 1),
+        ),
+      ];
+    },
+  });
+
+  const document = vscode.commands.registerCommand(
+    "extension.gotoDocument",
+    (e: vscode.Uri) => {
+      let editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.showWarningMessage("No active text editor found!");
+        return;
+      }
+
+      let keyword: string = "";
+      if (editor.selection.isEmpty) {
+        let wordRange: any = editor.document.getWordRangeAtPosition(
+          editor.selection.start,
+        );
+        keyword = wordRange ? editor.document.getText(wordRange) : "";
+      } else {
+        keyword = editor.document.getText(editor.selection.with());
+      }
+      let extIndex: number = editor.document.fileName.lastIndexOf(".");
+      let ext: string =
+        extIndex >= 0 ? editor.document.fileName.substring(extIndex + 1) : "";
+      let config = vscode.workspace.getConfiguration("goto-documentation");
+      let customDocs = config.get<object>("customDocs");
+
+      Document.open(ext, keyword, customDocs);
+    },
+  );
+
   const hover = vscode.languages.registerHoverProvider("python", {
     provideHover(document, position, token) {
       const wordRange = document.getWordRangeAtPosition(position);
@@ -219,7 +265,7 @@ export function activate(context: vscode.ExtensionContext) {
     );
   });
 
-  context.subscriptions.push(keywords, hover);
+  context.subscriptions.push(keywords, hover, document, definition);
 }
 
 export function deactivate() {}
